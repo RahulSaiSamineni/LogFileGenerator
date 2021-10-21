f454fb2682345c3f58b5a0a442320ecf7b5249b9
Rahul Sai Samineni <rsamin4@uic.edi>
2021-10-20 23:53:35 -0500

    commit

---
+++ src/main/test/MapReduce/MRTests.scala
@@ -0,0 +1,14 @@
+package MapReduce
+
+import org.scalatest.funsuite.AnyFunSuite
+import HelperUtils.ObtainConfigReference
+import MapReduce.Job4
+
+class MRTests extends AnyFunSuite{
+  test("Count of each type of error"){
+    val s = map(input,value)
+    val r = reducer(s)
+  }
+
+
+}
